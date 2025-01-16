5862c205878784f389e4dafb8ab217db9e072c57
Maxim Tugunchinov <tugunchinov.maxim@gmail.com>
2025-01-16 14:29:22 +0300

    add compression

--- src/lib.rs
+++ src/lib.rs
@@ -20,17 +20,21 @@ pub fn create_patch(
     let patch = crate::patch::create(old_bytes, new_bytes)?;
 
     let serialized_patch = patch.into_bytes();
+    let compressed = miniz_oxide::deflate::compress_to_vec(&serialized_patch, 6);
 
-    std::fs::write(output, serialized_patch)
+    std::fs::write(output, compressed)
 }
 
 pub fn apply_patch(file: PathBuf, patch: PathBuf, output: Option<PathBuf>) -> std::io::Result<()> {
     let output = output.unwrap_or(DEFAULT_OUTPUT_FILE_NAME.into());
 
     let bytes = std::fs::read(file)?;
-    let serialized_patch = std::fs::read(&patch)?;
 
-    let patch = Patch::from_bytes(&serialized_patch)?;
+    let serialized_patch = std::fs::read(&patch)?;
+    let decompressed = miniz_oxide::inflate::decompress_to_vec(&serialized_patch)
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e.to_string()))?;
+
+    let patch = Patch::from_bytes(&decompressed)?;
 
     let result = crate::patch::apply(bytes, patch)?;
 
